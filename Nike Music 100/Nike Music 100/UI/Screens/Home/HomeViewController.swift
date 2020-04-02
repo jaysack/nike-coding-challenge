c1c2cf04c9773dca0dfdf3b68c0a78a13eb7ad12
Jonathan Sack <jonathan.sack91@gmail.com>
2020-04-02 08:57:35 -0500

    [Jonathan]: Set up project environment

---
+++ Nike Music 100/Nike Music 100/UI/Screens/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Nike Music 100
+//
+//  Created by Jonathan Sack on 4/2/20.
+//  Copyright © 2020 Jonathan Sack. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+}
+
